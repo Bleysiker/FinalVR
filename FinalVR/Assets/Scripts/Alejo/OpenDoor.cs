76430660e36f7d7c7a4f2104856da6502b4e0d5c
Alejo <87954897+alejolop117@users.noreply.github.com>
2022-10-12 11:09:24 -0500

    Script anim

---
+++ FinalVR/Assets/Scripts/Alejo/OpenDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    
+    public void Open()
+    {
+
+    }
+
+    public void Close()
+    {
+
+    }
+   
+}
