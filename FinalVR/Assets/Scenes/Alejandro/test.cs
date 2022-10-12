96e8025577cc74570981067e8630aed3dc9663ae
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-10-12 00:24:57 -0500

    Test apertura cuarto secreto

---
+++ FinalVR/Assets/Scenes/Alejandro/test.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public void AbrirCamara() {
+        Debug.Log("SE ABREEEE");
+        transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+    }
+}
