2f5cdeb26a7db48aae2725e7d70c7a93b70c82a8
Juan Andres Osorio <jaot12345@gmail.com>
2022-10-14 11:53:56 -0500

    Funcionamiento reloj y Escena Inicial
    Incorpore un canvas de hijo de la cabeza, el cual se mueve con el usuario para cuando este active el mapa. Inicia desactivado. Integre un cubo el cual utiliza un follower para ubicarse en la muñeca derecha con el offset que esta de hijo este. El objeto es interactable y cuando se entra en contacto por primer vez con la mano izquiera este activa el canvas, el canvas es interactuable e incorporta un boton para desactivarlo. Ademas de esto, el reloj tiene un collsion ignorer para evitar que se active al contacto con el controllable derecho.

---
+++ FinalVR/Assets/Scripts/Juan/MapManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject canvas;
+
+    public void canvasButton()
+    {
+        canvas.SetActive(!canvas.activeSelf);
+    }
+    public void canvasActivateReloj()
+    {
+        canvas.SetActive(true);
+    }
+}
