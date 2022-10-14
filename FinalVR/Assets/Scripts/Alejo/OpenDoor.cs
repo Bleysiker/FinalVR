44855e442ad20728d68c1e6d22d835f14f50b283
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-10-13 23:02:19 -0500

    Animación puerta secreta
    C# + anim

--- FinalVR/Assets/Scripts/Alejo/OpenDoor.cs
+++ FinalVR/Assets/Scripts/Alejo/OpenDoor.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
-    
+    [SerializeField] Animator m_anim;   
     public void Open()
     {
-
+        m_anim.SetBool("isOpen", true);
     }
 
     public void Close()
     {
-
+        m_anim.SetBool("isOpen", false);
     }
    
 }
