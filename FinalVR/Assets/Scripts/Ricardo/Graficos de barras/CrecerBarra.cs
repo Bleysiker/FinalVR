9178a7e4e9c7ed2320a822dd087e24da1742d23c
Ricardo <henry3197@hotmail.com>
2022-10-26 00:32:51 -0500

    graficos programados

---
+++ FinalVR/Assets/Scripts/Ricardo/Graficos de barras/CrecerBarra.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrecerBarra : MonoBehaviour
+{
+    [SerializeField] Image barra;
+    [SerializeField] Color ganancia, perdida;
+    [SerializeField] bool recaudo;
+    [SerializeField] float tiempoBarra,tamanofinal;
+    [SerializeField] REvents aparecer;
+    [SerializeField] CanvasGroup tituloBarra;
+    void Start()
+    {
+        transform.transform.LeanScaleX(0.01f, 0f).setEaseInOutQuart();
+        aparecer.GEvent += EstablecerBarra;
+        tituloBarra.alpha = 0;
+    }
+
+    void EstablecerBarra()
+    {
+        transform.LeanScaleX(tamanofinal, tiempoBarra).setEaseInOutQuart();
+        tituloBarra.LeanAlpha(1f, tiempoBarra/2);
+    }
+    private void Update()
+    {
+        if (recaudo == false)
+        {
+            if (transform.transform.localScale.x < 1)
+            {
+                barra.color = perdida;
+            }
+            else
+            {
+                barra.color = ganancia;
+            }
+        }
+    }
+    private void OnDestroy()
+    {
+        aparecer.GEvent -= EstablecerBarra;
+    }
+}
